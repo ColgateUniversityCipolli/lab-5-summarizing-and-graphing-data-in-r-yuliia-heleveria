8a11d136c3692e39742e6728f3cf4bc40c397311
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-02-23 17:58:07 -0500

    Fixed Step 1 and started working on the function

--- Lab5.R
+++ Lab5.R
@@ -1,4 +1,4 @@
-#Write headers?
+#Write headers!!!
 
 
 #load packages
@@ -9,14 +9,15 @@ library("tidyverse")
 #Part 1 - group data by artist
 #load data frames for all tracks and allentown
 essentia.data.allentown <- read_csv("data/essentia.data.allentown.csv")
-essentia.data <- read_csv("data/essentia.data.csv") %>%
+essentia.data <- read_csv("data/essentia.data.csv") 
+essentia.statistics <- essentia.data |>
   group_by(artist) |>
   summarize(min = min(overall_loudness), #Part 2 - summarize the data by computing statistics
             LF = quantile(overall_loudness, 0.25) - 1.51*IQR(overall_loudness), 
             UF = quantile(overall_loudness, 0.75) + 1.51*IQR(overall_loudness), 
             max = max(overall_loudness)) |>
   #Part 3 - Create two new columns - out of range and unusual
-  mutate(out.of.range =  if_else(essentia.data.allentown$overall_loudness < min | 
+  mutate(out.of.range = if_else(essentia.data.allentown$overall_loudness < min | 
                                    essentia.data.allentown$overall_loudness > max,
                                  T, F)) |>
   mutate(unusual =  if_else(essentia.data.allentown$overall_loudness < LF | 
@@ -24,12 +25,38 @@ essentia.data <- read_csv("data/essentia.data.csv") %>%
                                  T, F)) |>
   #Part 4 - create a column description
   rowwise()|> #apply row by row
-  mutate(description = if (out.of.range){
-    description = "Out of Range"
-    }else if(unusual){
-      description = "Outlying"
-    }else{
-      description = "Within Range"
-    })
-  
-  
+  mutate(description = case_when(
+    out.of.range ~ "Out of Range",
+    unusual ~ "Outlying",
+    TRUE ~ "Within Range"))|>
+  ungroup()
+
+#Create the function for analyzing each feature
+for (i in 1:length(essentia.data)){
+  print(i)
+}
+
+feature_analysis <- function(feature){
+  essentia.statistics <- essentia.data |>
+    group_by(artist) |>
+    summarize(min = min(overall_loudness), #Part 2 - summarize the data by computing statistics
+              LF = quantile(overall_loudness, 0.25) - 1.51*IQR(overall_loudness), 
+              UF = quantile(overall_loudness, 0.75) + 1.51*IQR(overall_loudness), 
+              max = max(overall_loudness)) |>
+    #Part 3 - Create two new columns - out of range and unusual
+    mutate(out.of.range = if_else(essentia.data.allentown$overall_loudness < min | 
+                                    essentia.data.allentown$overall_loudness > max,
+                                  T, F)) |>
+    mutate(unusual =  if_else(essentia.data.allentown$overall_loudness < LF | 
+                                essentia.data.allentown$overall_loudness > UF,
+                              T, F)) |>
+    #Part 4 - create a column description
+    rowwise()|> #apply row by row
+    mutate(description = case_when(
+      out.of.range ~ "Out of Range",
+      unusual ~ "Outlying",
+      TRUE ~ "Within Range"))|>
+    ungroup()
+}
+
+feature_analysis(overall_loudness)
