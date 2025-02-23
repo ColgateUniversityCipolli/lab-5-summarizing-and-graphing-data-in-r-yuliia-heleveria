0e5d8924fddff35a20b21c81b3310842cc32d629
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-02-23 18:14:23 -0500

    Finished function for analyzing features

--- Lab5.R
+++ Lab5.R
@@ -36,19 +36,20 @@ for (i in 1:length(essentia.data)){
   print(i)
 }
 
+#function for analyzing each feature of the dataset
 feature_analysis <- function(feature){
   essentia.statistics <- essentia.data |>
     group_by(artist) |>
-    summarize(min = min(overall_loudness), #Part 2 - summarize the data by computing statistics
-              LF = quantile(overall_loudness, 0.25) - 1.51*IQR(overall_loudness), 
-              UF = quantile(overall_loudness, 0.75) + 1.51*IQR(overall_loudness), 
-              max = max(overall_loudness)) |>
+    summarize(min = min(get(feature)), #Part 2 - summarize the data by computing statistics
+              LF = quantile(get(feature), 0.25) - 1.51*IQR(get(feature)), 
+              UF = quantile(get(feature), 0.75) + 1.51*IQR(get(feature)), 
+              max = max(get(feature))) |>
     #Part 3 - Create two new columns - out of range and unusual
-    mutate(out.of.range = if_else(essentia.data.allentown$overall_loudness < min | 
-                                    essentia.data.allentown$overall_loudness > max,
+    mutate(out.of.range = if_else(essentia.data.allentown[[feature]] < min | 
+                                    essentia.data.allentown[[feature]] > max,
                                   T, F)) |>
-    mutate(unusual =  if_else(essentia.data.allentown$overall_loudness < LF | 
-                                essentia.data.allentown$overall_loudness > UF,
+    mutate(unusual =  if_else(essentia.data.allentown[[feature]] < LF | 
+                                essentia.data.allentown[[feature]] > UF,
                               T, F)) |>
     #Part 4 - create a column description
     rowwise()|> #apply row by row
@@ -59,4 +60,4 @@ feature_analysis <- function(feature){
     ungroup()
 }
 
-feature_analysis(overall_loudness)
+stats.feature <- feature_analysis("overall_loudness")
